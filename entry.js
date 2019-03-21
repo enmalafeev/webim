5257d0b5dd185abc6f6eb02295b4bfbbb9318073
enmalafeev <malafeev.en@yandex.ru>
2019-03-21 18:59:03 +0300

    add btn for oauth

--- entry.js
+++ entry.js
@@ -1,15 +1,20 @@
 import Model from './model.js';
 import View from './view.js'
 
-(async () => {
+const btnOauth = document.getElementById('btn-oauth');
+btnOauth.addEventListener('click', async () => {
   try {
     const header = document.querySelector('#header');
     await Model.login(6907858, 2);
-    const [me] = await Model.getUser({ name_case: 'gen' });
+    const [me] = await Model.getUser({
+      name_case: 'gen'
+    });
 
     header.innerHTML = View.render('header', me);
   } catch (e) {
     console.log(e);
     alert('Ошибка: ' + e.message);
   }
-})();
+}
+);
+
