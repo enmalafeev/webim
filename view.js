4127d117dc37e13a6d60487d51baf32a34ce74e8
enmalafeev <malafeev.en@yandex.ru>
2019-03-21 07:45:36 +0300

    add entry.js and view.js

---
+++ view.js
@@ -0,0 +1,12 @@
+export default {
+  render(templateName, data) {
+    templateName = templateName + 'Template';
+
+    const templateElement = document.getElementById('header');
+    const templateSource = templateElement.innerHTML;
+    const renderFn = Handlebars.compile(templateSource);
+
+    return renderFn(data);
+
+  }
+}
