a03576229a7f17be183b092acbdd316524197918
enmalafeev <malafeev.en@yandex.ru>
2019-03-24 18:59:00 +0300

    add handlebars registerHelper

--- entry.js
+++ entry.js
@@ -1,3 +1,27 @@
+Handlebars.registerHelper('formatTime', time => {
+  let minutes = (time / 60).toFixed();
+  let seconds = time - minutes * 60;
+
+  minutes = minutes.toString().length === 1 ? '0' + minutes : minutes;
+  seconds = seconds.toString().length === 1 ? '0' + seconds : seconds;
+
+  return minutes + ':' + seconds;
+});
+Handlebars.registerHelper('formatBDay', bday => {
+  const months = [
+    'января', 'февраля', 'марта',
+    'апреля', 'мая', 'июня',
+    'июля', 'августа', 'сентября',
+    'октября', 'ноября', 'декабря'
+  ];
+  const [day, month] = (bday || '').split('.');
+
+  return [day, months[month - 1]].join(' ');
+});
+Handlebars.registerHelper('formatDate', ts => {
+  return new Date(ts * 1000).toLocaleString();
+});
+
 import Model from './model.js';
 import View from './view.js'
 import Router from './router.js';
